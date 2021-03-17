506831e0d18aad742e8c9c447f73cbfc8b88facd
Nattaphat Thongsan <Nattaphat5984@gmail.com>
2021-03-17 17:05:52 +0700

    add switch and func to file

--- h1/switch.php
+++ h1/switch.php
@@ -1,16 +1,64 @@
+Skip to content
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@Nattafolk 
+Learn Git and GitHub without any code!
+Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.
+
+
+Jatumongkon62101233
+/
+codephp62
+1
+00
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+codephp62/hw1/switch.php /
+@Jatumongkon62101233
+Jatumongkon62101233 func
+Latest commit a227aab 18 minutes ago
+ History
+ 1 contributor
+29 lines (28 sloc)  458 Bytes
+  
 <?php
-$a = 5;
+$a = 49;
 switch ($a) {
-    case 5:
-        echo "a equal 5";
+    case ($a < 50):
+        echo "F";
        break;
-    case ($a > 3 && $a < 6):
-        echo "a gt 5";
+    case ($a <55):
+        echo "D";
         break;
-    case ($a < 5):
-        echo "a less 5";
+    case ($a <60):
+        echo "D+";
+        break;
+    case ($a <65):
+        echo "C";
+        break;
+    case ($a <70):
+        echo "C+";
+        break; 
+    case ($a <75):
+        echo "B";
+        break;   
+        
+    case ($a < 80):
+        echo "B+";
         break;
     default:
-        echo "default";
+        echo "A";
         break;
 }
+
--- hw2/func.php
+++ hw2/func.php
@@ -1,54 +1,42 @@
+  
 <?php
-// myFunction();
 
-function myFunction()
+function GPA($score = 56)
 {
-    echo "my1";
-}
-
-function functon2()
+switch ($score) 
 {
-    echo "my";
 
-    return "you call me";
-}
-//$calFunc = functon2();
-
-//echo $calFunc;
-
-function calnumber($num1, $num2 = 7)
-{
-    $a = $num1;
-    $b = $num2;
-    $c = $a + $b;
-    $num1 = 6;
-    return $c;
-}
-//// $num1 = 5;
-// $num2 = 6;
-// echo calnumber($num1,9);
-// echo $num1;
-function fatorail($num)
-{
-    if ($num == 0 || $num == 1) {
-        return 1;
-    } else {
-        return $num * fatorail($num - 1);
+    case ($score < 50):
+        echo "F";
+       break;
+    case ($score <55):
+        echo "D";
+        break;
+    case ($score <60):
+        echo "D+";
+        break;
+    case ($score <65):
+        echo "C";
+        break;
+    case ($score <70):
+        echo "C+";
+        break; 
+    case ($score <75):
+        echo "B";
+        break;   
+        
+    case ($score < 80):
+        echo "B+";
+        break;
+    default:
+        echo "A";
+        break;
     }
+    echo "<br>";
+    return $score;
+
 }
-// echo fatorail(5) . "<br>";
-
-// $Fullname = "Mr.Suradet Petcharanon";
-// $lastname = substr($Fullname,11,3);
-// echo $lastname;
-// echo strlen($lastname);
-// echo "<br>";
-// echo max(1,4.33,45);
-// echo round(19.987777,2);
-
-echo date("d/m/Y", "1579737600");
 echo "<br>";
-// echo getdate();
-$currentdate = getdate();
-echo $currentdate["mday"];
-//echo time();
+echo GPA()
+
+?>
